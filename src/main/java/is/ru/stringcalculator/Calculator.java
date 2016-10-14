2c1a29632b576ed339563dcdd2b970a2ae43d518
Bebelingur <berglind15@ru.is>
2016-10-14 10:40:35 +0000

    Refactor: making a private function for summing numbers in text array

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,23 +1,35 @@
 package is.ru.stringcalculator;
 
 public class Calculator {
-	public static int add(String text) {
-			if(text.equals(""))
-				return 0;
-			else if(text.contains(",")){
-				String [] numbers = splitNumbers(text);
-				return toInt(numbers[0]) + toInt(numbers[1]);
-			}
-			else
-				return 1;
+	public static int add(String text) 
+	{
+		if(text.equals(""))
+			return 0;
+		else if(text.contains(",")){
+			return sum(splitNumbers(text));
+		}
+		else
+			return 1;
 	}
 
-	private static int toInt(String text){
+	private static int toInt(String text)
+	{
 		return Integer.parseInt(text);
 	}
 
-	private static String [] splitNumbers(String text){
+	private static String [] splitNumbers(String text)
+	{
 		return text.split(",");
+	}
+
+	private static int sum(String [] array)
+	{	
+		int totalSum = 0;
+		for(String number : array)
+		{
+			totalSum += toInt(number);
+		}
+		return totalSum;
 	}
 
 }
